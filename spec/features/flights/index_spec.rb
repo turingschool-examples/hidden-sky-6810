17220fc501641f66efe94030b0a9540682bb05ff
Christian Valesares <cvalesares@gmail.com>
2021-11-16 11:34:51 -0500

    add relationships and flight index page

---
+++ spec/features/flights/index_spec.rb
@@ -0,0 +1,30 @@
+require 'rails_helper'
+
+RSpec.describe "Flight Index Page" do
+  before :each do
+    @airline1 = Airline.create!(name: "Southwest")
+    @flight1 = @airline1.flights.create!(number: 1234, date: "06/06/22", departure_city: "Denver", arrival_city: "Tampa")
+    @flight2 = @airline1.flights.create!(number: 1567, date: "07/02/22", departure_city: "Chicago", arrival_city: "LA")
+    @passenger1 = @flight1.passengers.create!(name: "Derek", age: 37)
+    @passenger2 = @flight1.passengers.create!(name: "Colt", age: 17)
+    @passenger3 = @flight2.passengers.create!(name: "Larry", age: 53)
+  end
+
+  it "shows flights and their attributes" do
+    visit "/flights"
+    save_and_open_page
+    expect(page).to have_content(@flight1.number)
+    expect(page).to have_content(@flight2.number)
+
+    within "#id-#{@flight1.id}" do
+      expect(page).to have_content(@passenger1.name)
+      expect(page).to have_content(@passenger2.name)
+      expect(page).to have_content(@airline1.name)
+    end
+
+    within "#id-#{@flight2.id}" do
+      expect(page).to have_content(@passenger3.name)
+      expect(page).to have_content(@airline1.name)
+    end
+  end
+end
