02b798976aca5df67b54315204ceb16104b757fa
ChloeLL <chloell@protonmail.ch>
2021-11-16 11:29:43 -0700

    Remove pry

--- app/controllers/flight_passengers_controller.rb
+++ app/controllers/flight_passengers_controller.rb
@@ -1,6 +1,5 @@
 class FlightPassengersController < ApplicationController
   def destroy
-    binding.pry
     pass = FlightPassenger.find_by(flight_id: params[:flight_id], passenger_id: params[:id])
     pass.destroy
 
