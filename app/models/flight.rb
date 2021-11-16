539629ff0e6ca93f8ef2308289dd321ab04c6842
Devin Pile <devinpile@gmail.com>
2021-11-16 09:31:06 -0700

    Initial commit with db setup: migrations,relations,validations

--- app/models/flight.rb
+++ app/models/flight.rb
@@ -1,3 +1,10 @@
 class Flight < ApplicationRecord
   belongs_to :airline
+  has_many :flight_passengers
+  has_many :passengers, through: :flight_passengers
+
+  validates_presence_of :number
+  validates_presence_of :date
+  validates_presence_of :departure_city
+  validates_presence_of :arrival_city
 end
--- spec/models/airline_spec.rb
+++ spec/models/airline_spec.rb
@@ -1,5 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe Airline, type: :model do
-  it {should have_many :flights}
+  it { should have_many :flights }
+  
+  it { should validate_presence_of :name }
 end
